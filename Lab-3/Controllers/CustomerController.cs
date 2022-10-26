983cd72f78b728fa16ac22cf1a050ce21a10928a
Dawid Studziżba <Dawid.Studzizba@microsoft.wsei.edu.pl>
2022-10-26 14:58:56 +0200

    commit

---
+++ Lab-3/Controllers/CustomerController.cs
@@ -0,0 +1,40 @@
+﻿using Lab_3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_3.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        private static int _counter = 1;
+        private static List<Customer> _customers = new List<Customer>()
+        {
+            new Customer()
+            {
+                Email = "adam@op.pl", Id = _counter++, UserName = "Adam",
+                Age = 21, PhoneNumber = "123456789", Address = "Nowa Huta 15",
+                ZipCode = "31-236", Date = DateTime.Parse("2020-10-23"),
+            },
+            new Customer()
+            {
+                Email = "ola@google.pl", Id = _counter++, UserName = "Ola",
+                Age = 19, PhoneNumber = "123123123", Address = "Aleja 29 listopada 150",
+                ZipCode = "31-251", Date = DateTime.Now,
+            },
+            new Customer()
+            {
+                Email = "lukasz@wsei.edu.pl", Id = _counter++, UserName = "Ukasz",
+                Age = 27, PhoneNumber = "000000001", Address = "Nowy kleparz 55",
+                ZipCode = "31-263", Date = DateTime.Parse("2021-11-23"),
+            }
+        };
+        [HttpGet]
+        public string CreateCustomers()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lab-3/Models/Customer.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab_3.Models
+{
+    public class Customer
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Proszę podać nazwę!")]
+        public string UserName { get; set; }
+        [EmailAddress]
+        [Required(ErrorMessage = "Proszę podać poprawy Email!")]
+        public string Email { get; set; }
+
+        public int Age { get; set; }
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        public string Address { get; set; }
+        [RegularExpression(@"^[0-9]{2}-[0-9]{3}$")]
+        public string ZipCode { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+    }
+}
